bdb9665ed84a926637ebd8edcc564f45928b0911
stDem <61629062+stDem@users.noreply.github.com>
2025-05-10 21:27:34 +0200

    changed for deploy

--- HW2/assign2.py
+++
@@ -1,9 +0,0 @@
-def entferne_doppelteStudenten(studenten):
-    result = []
-    for student in studenten:
-        if student not in result:
-            result.append(student)
-    return result
-
-print(entferne_doppelteStudenten([1234, 1235, 1236, 1234, 1235])) 
-print(entferne_doppelteStudenten([1111, 1111, 1111, 1111])) 
